288b94a14e1dbf218d34dcd83c5745e5b4da3eaa
mcheung008 <mcheung008@gmail.com>
2020-12-25 11:30:45 -0800

    first commit

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+app.listen(PORT, () => console.log("Server is running on" + PORT));
